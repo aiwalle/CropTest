52b4edf63b504254ba31bb09f0c61aababa24459
aiwalle <wallelj0909@gmail.com>
2022-10-12 20:08:55 +0800

    调整代码

--- GestureDemo/TestController.m
+++ GestureDemo/TestController.m
@@ -14,6 +14,13 @@
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIView *imageContainerView;
 @property (nonatomic, strong) UIImageView *imageView;
+
+// 图片初始缩放系数
+@property (nonatomic, assign) float startScale;
+// 图片初始中心点
+@property (nonatomic) CGPoint startImageCenter;
+// 手势初始中心点
+@property (nonatomic) CGPoint startGCenter;
 @end
 
 @implementation TestController
@@ -41,16 +48,17 @@ static float testValue = 200;
     [self.scrollView addSubview:self.imageView];
     
     self.imageView.image = self.originImg;
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panImageView:)];
+    [self.imageView addGestureRecognizer:pan];
+    self.imageView.userInteractionEnabled = YES;
+    
 //    CGFloat testValue = 200;
-    self.scrollView.contentSize = CGSizeMake(imageViewRect.size.width + testValue, self.scrollView.frame.size.height + testValue);
-//    self.scrollView.contentOffset = CGPointMake(100, 100);
-    self.scrollView.contentInset = UIEdgeInsetsMake(testValue, testValue, testValue, testValue);
-//    self.scrollView.contentOffset = CGPointZero;
-//    CGFloat testValue = 200;
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        self.scrollView.contentSize = CGSizeMake(self.scrollView.contentSize.width + testValue, self.scrollView.contentSize.height + testValue);
-//        self.scrollView.contentInset = UIEdgeInsetsMake(testValue, testValue, testValue, testValue);
-//    });
+//    self.scrollView.contentSize = CGSizeMake(imageViewRect.size.width + testValue, self.scrollView.frame.size.height + testValue);
+////    self.scrollView.contentOffset = CGPointMake(100, 100);
+//    self.scrollView.contentInset = UIEdgeInsetsMake(testValue, testValue, testValue, testValue);
+
 }
 
 - (UIScrollView *)scrollView {
@@ -94,21 +102,21 @@ static float testValue = 200;
 }
 
 - (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view {
-//    scrollView.contentInset = UIEdgeInsetsZero;
+    scrollView.contentInset = UIEdgeInsetsZero;
 }
 
 - (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(nullable UIView *)view atScale:(CGFloat)scale {
-    CGFloat contentWidth = scrollView.frame.size.width + testValue;
-    CGFloat contentHeight = scrollView.frame.size.height + testValue;
-    if (scrollView.contentSize.width > contentWidth) {
-        contentWidth = scrollView.contentSize.width + testValue;
-    }
-    
-    if (scrollView.contentSize.height > contentHeight) {
-        contentHeight = scrollView.contentSize.height + testValue;
-    }
-    
-    scrollView.contentSize = CGSizeMake(contentWidth, contentHeight);
+//    CGFloat contentWidth = scrollView.frame.size.width + testValue;
+//    CGFloat contentHeight = scrollView.frame.size.height + testValue;
+//    if (scrollView.contentSize.width > contentWidth) {
+//        contentWidth = scrollView.contentSize.width + testValue;
+//    }
+//
+//    if (scrollView.contentSize.height > contentHeight) {
+//        contentHeight = scrollView.contentSize.height + testValue;
+//    }
+//
+//    scrollView.contentSize = CGSizeMake(contentWidth, contentHeight);
     
 //    if (scale > 2) {
 //        CGFloat width = scrollView.contentSize.width;
@@ -135,6 +143,26 @@ static float testValue = 200;
     [self refreshImageContainerViewCenter];
 }
 #pragma mark - Private
+- (void)panImageView:(UIPanGestureRecognizer *)pan {
+    UIImageView * imageView = (UIImageView *)pan.view;
+    
+    if (pan.state == UIGestureRecognizerStateBegan) {
+        
+        //记录中心位置
+        self.startImageCenter = imageView.center;
+        self.startGCenter = [pan locationInView:self.view];
+        return;
+    }
+    
+    //非开始阶段
+    //获得手势移动的距离 现在的位置
+    CGPoint nowGCenter = [pan locationInView:self.view];
+    float x = nowGCenter.x - self.startGCenter.x;
+    float y = nowGCenter.y - self.startGCenter.y;
+    
+    //计算imageview的相对位移
+    imageView.center = CGPointMake(self.startImageCenter.x+x, self.startImageCenter.y+y);
+}
 
 - (void)refreshImageContainerViewCenter {
     CGFloat offsetX = (_scrollView.frame.size.width > _scrollView.contentSize.width) ? ((_scrollView.frame.size.width - _scrollView.contentSize.width) * 0.5) : 0.0;
